20d86fd6772da87db73949f4ed8bac49ad927bf6
Docun <167471277+Docun@users.noreply.github.com>
2024-05-04 16:11:49 +0300

    Edits_genre

--- tests.py
+++ tests.py
@@ -12,39 +12,39 @@ class TestBooksCollector:
     def test_set_book_genre_sets_correct_genre(self):
         collector = BooksCollector()
         collector.add_new_book('Сто лет тому вперед')
-        collector.set_book_genre('Сто лет тому вперед', 'повесть')
-        assert collector.get_book_genre('Сто лет тому вперед') == 'повесть'
+        collector.set_book_genre('Сто лет тому вперед', 'Детективы')
+        assert collector.get_book_genre('Сто лет тому вперед') == 'Детективы'
 
     def test_get_book_genre_returns_correct_genre(self):
         collector = BooksCollector()
         collector.add_new_book('Сёгун')
-        collector.set_book_genre('Сёгуне', 'роман')
-        assert collector.get_book_genre('Сёгун') == 'роман'
+        collector.set_book_genre('Сёгун', 'Фантастика')
+        assert collector.get_book_genre('Сёгун') == 'Фантастика'
 
     def test_get_books_with_specific_genre_returns_list_of_books(self):
         collector = BooksCollector()
         collector.add_new_book('Дюна')
-        collector.set_book_genre('Дюна', 'фентази')
-        collector.add_new_book('преступление и наказание')
-        collector.set_book_genre('Преступление и наказание', 'детектив')
-        assert collector.get_books_with_specific_genre('фентази') == ['Дюна']
-        assert collector.get_books_with_specific_genre('детектив') == ['преступление и наказание']
+        collector.set_book_genre('Дюна', 'Фантастика')
+        collector.add_new_book('Преступление и наказание')
+        collector.set_book_genre('Преступление и наказание', 'Детективы')
+        assert collector.get_books_with_specific_genre('Фантастика') == ['Дюна']
+        assert collector.get_books_with_specific_genre('Детективы') == ['Преступление и наказание']
 
     def test_get_books_genre_returns_books_genre_dict(self):
         collector = BooksCollector()
         collector.add_new_book('Чудесный отец')
-        collector.set_book_genre('Чудесный отец', 'повесть')
+        collector.set_book_genre('Чудесный отец', 'Фантастика')
         collector.add_new_book('Преступление и наказание')
-        collector.set_book_genre('Преступление и наказание', 'детектив')
-        assert collector.get_books_genre() == {'Чудесный отец': 'повесть', 'преступление и наказание': 'детектив'}
+        collector.set_book_genre('Преступление и наказание', 'Детективы')
+        assert collector.get_books_genre() == {'Чудесный отец': 'Фантастика', 'Преступление и наказание': 'Детективы'}
 
     def test_get_books_for_children_returns_books_without_age_rating(self):
         collector = BooksCollector()
-        collector.add_new_book('гоблинские истории')
-        collector.set_book_genre('гоблинские истории', 'фэнтези')
-        collector.add_new_book('золушка')
-        collector.set_book_genre('золушка', 'сказка')
-        assert collector.get_books_for_children() == ['гоблинские истории', 'золушка']
+        collector.add_new_book('Гоблинские истории')
+        collector.set_book_genre('Гоблинские истории', 'Фантастика')
+        collector.add_new_book('Золушка')
+        collector.set_book_genre('Золушка', 'Мультфильмы')
+        assert collector.get_books_for_children() == ['Гоблинские истории', 'Золушка']
 
     def test_add_book_in_favorites_adds_book_to_favorites(self):
         collector = BooksCollector()
